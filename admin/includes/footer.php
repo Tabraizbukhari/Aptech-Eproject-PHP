1eafb490f21cb9286651777230c3a898794399ce
Tabraiz Bukhari <m.tabraizbukhari@gmail.com>
2020-06-03 15:20:52 +0500

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,19 @@
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.0.2
+    </div>
+    <strong>Copyright &copy; 2020 <a href="#TabnaizBukhari">Tabraiz Bukhari</a>.</strong> All rights
+    reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+   <?php include 'script.php' ?>
+</body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,11 @@
+ <?php include 'includes/header.php' ?>
+ <?php include 'includes/navbar.php' ?>
+ <?php include 'includes/sidebar.php' ?>
+
+ <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+   
+  </div>
+  <!-- /.content-wrapper -->
+
+ <?php include 'includes/footer.php' ?>
